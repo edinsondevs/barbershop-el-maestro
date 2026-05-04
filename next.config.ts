fab95310c5b471115df7c1a2cc7e7d3b77591233
Edinson Madrid <edinsonmadrid@gmail.com>
2026-05-03 21:46:02 -0300

    fix: remove webpack config for vercel build

--- next.config.ts
+++ next.config.ts
@@ -14,22 +14,7 @@ const nextConfig: NextConfig = {
 		formats: ["image/webp", "image/avif"],
 	},
 	output: "standalone",
-	// Esta parte es vital para el Hot Reload a través de la red de Docker
-	webpack: (config, { dev, isServer }) => {
-		if (dev && !isServer) {
-			config.watchOptions = {
-				poll: 100, // Revisa cambios cada 500ms (más rápido)
-				aggregateTimeout: 100, // Tiempo de espera desde que dejas de escribir hasta que compila
-				// Evita que Webpack escanee estas carpetas, ahorrando mucho tiempo
-				ignored: [
-        		  '**/node_modules',
-        		  '**/.next',
-        		  '**/.git'
-        		],
-			};
-		}
-		return config;
-	},
+	turbopack: {},
 };
 
 export default nextConfig;
